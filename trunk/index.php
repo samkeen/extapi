9706bb7b57dbd1e4b23312c1253706a9996bc81f
sam.sjk <sam.sjk@700042d9-be52-0410-a9f9-311eb4db76ec>
2008-10-23 04:49:48 +0000

    run custom logger in web root

--- trunk/index.php
+++ trunk/index.php
@@ -1,6 +1,6 @@
 <?php
 require 'app/bootstrap.php';
-$logger = new Logger(Logger::DEBUG);
+$logger = new Logger(Logger::DEBUG,dirname(__FILE__).'/extapi.log');
 
 $custom_routes = isset($custom_routes)?$custom_routes:null;
 $router = new Util_Router($custom_routes);

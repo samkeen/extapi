df6579927f2da885cf099aa6ac75c67313e863c8
sam.sjk <sam.sjk@700042d9-be52-0410-a9f9-311eb4db76ec>
2008-11-12 23:22:59 +0000

    refactored

--- trunk/aabot/Logger.php
+++ trunk/aabot/Logger.php
@@ -3,23 +3,37 @@
  * Very simple Logging class.  Allowed you to add debugging statments in code and
  * "turn them off" in production
  * 
+ * any of the logging methods can be used as a check to see if that level is enabled.
+ * Usefull to save the work of building complex log entries when that level is not 
+ * even enabled.  ex:
+ * <code>
+ * if($logger->debug()) {
+ * 		$logger->debug(__METHOD__. 'User:'.get_user($id).' was not found in primary env: '
+ * 			.get_primary_env($request). ' or in secondary ' . get_secondaty_env());
+ * }
+ * </code> 
+ * 
  * @author "Sam Keen" <sam@pageindigo.com>
  * 
  */
 class Logger {
 	
-	public $log_level;
+	private $current_log_level;
 	const DEBUG = 40;
 	const NOTICE = 30;
 	const WARN = 20;
 	const ERROR = 10;
+	/*
+	 * this is used to start log statements with for instance: "[DEBUG]..."
+	 */
 	private $error_type_human = array(
 		self::DEBUG => "DEBUG",
 		self::NOTICE => "NOTICE",
 		self::WARN => "WARN",
 		self::ERROR => "ERROR"
 	);
-	private $maintain_buffer_of_statements;
+	
+	private $maintain_buffer_of_statements = false;
 	private $buffered_statements = array();
 	/*
 	 * hold the optional full path to the log file.  If not defined, we log to 
@@ -29,59 +43,67 @@ class Logger {
 	/**
 	 * @param $log_level The level at which to log.
 	 * @param $log_file_path If given we will attempt to write
-	 * log statments to that file, if not given we will write to the 
-	 * system log
+	 * log statments to that file, if not given we will write to the system log
+	 * $maintain_buffer_of_statements If true all log statments are save in a buffer that
+	 * can be recalled with $this->buffered_statements()
 	 */
 	public function __construct($log_level, $log_file_path=null, $maintain_buffer_of_statements=false) {
 		$this->log_file_path = $log_file_path;
-		$this->log_level = $log_level;
+		$this->current_log_level = $log_level;
 		$this->maintain_buffer_of_statements = $maintain_buffer_of_statements;
 	}
-	/**
-	 * @return boolean Tells you if the Debug level is turned on. Used
-	 * to wrapp expensive expressions that are building debug statments.
-	 */
-	public function debugEnabled() {
-		return $this->log_level >= self::DEBUG;
-	}
+
 	/**
 	 * Log a message at debug level
 	 * @param $log_message The message to log
+	 * If $log_message is null, then the caller of the method is just
+	 * checking if this log level is active (reason for the return value)
 	 */
-	public function debug($log_message) {
-		if ($this->log_level >= self::DEBUG) {
-			$this->logMessage($log_message,self::DEBUG);
-		}
+	public function debug($log_message=null) {
+		return $this->log_for_level(self::DEBUG, $log_message);
 	}
 	/**
 	 * Log a message at info level
 	 * @param $log_message The message to log
+	 * If $log_message is null, then the caller of the method is just
+	 * checking if this log level is active (reason for the return value)
 	 */
-	public function notice($log_message) {
-		if ($this->log_level >= self::NOTICE) {
-			$this->logMessage($log_message,self::NOTICE);
-		}
+	public function notice($log_message=null) {
+		return $this->log_for_level(self::NOTICE, $log_message);
 	}
 	/**
 	 * Log a message at info level
 	 * @param $log_message The message to log
+	 * If $log_message is null, then the caller of the method is just
+	 * checking if this log level is active (reason for the return value)
 	 */
-	public function warn($log_message) {
-		if ($this->log_level >= self::WARN) {
-			$this->logMessage($log_message,self::WARN);
-		}
+	public function warn($log_message=null) {
+		return $this->log_for_level(self::WARN, $log_message);
 	}
 	/**
 	 * Log a message at error level
 	 * @param $log_message The message to log
+	 * If $log_message is null, then the caller of the method is just
+	 * checking if this log level is active (reason for the return value)
 	 */
-	public function error($log_message) {
-		if ($this->log_level >= self::ERROR) {
-			$this->logMessage($log_message,self::ERROR);
-		}
+	public function error($log_message=null) {
+		return $this->log_for_level(self::ERROR, $log_message);
 	}
 	public function buffered_statements() {
 		return $this->buffered_statements;
+	}
+	/**
+	 * Log a message at error level
+	 * @param $level The level at which to attempt to log
+	 * @param $log_message The message to log
+	 * If $log_message is null, then the caller of the method is just
+	 * checking if this log level is active (reason for the return value)
+	 */
+	private function log_for_level($level, $log_message) {
+		if ($this->current_log_level >= $level && $log_message!==null) {
+			$this->logMessage($log_message, $level);
+		}
+		return $this->current_log_level >= $level; 
 	}
 	/**
 	 * log message if we are set at or above the $log_level supplied
@@ -96,7 +118,6 @@ class Logger {
 		if ($this->maintain_buffer_of_statements) {
 			$this->buffered_statements[] = $message;
 		}
-		
 	}
 	private function writeSystemMessage($log_message) {
 		error_log($log_message);
@@ -117,7 +138,4 @@ class Logger {
 			error_log("+!+!+!+!+!+[".__METHOD__."] LOG FILE ({$this->log_file_path}) NOT WRITABLE!!! ");
 		}
 	}
-	
-	
 }
-?>

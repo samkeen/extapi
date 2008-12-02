0cccd6802ce2f1a78b8b55977c98f0556aed5d1b
sam.sjk <sam.sjk@700042d9-be52-0410-a9f9-311eb4db76ec>
2008-12-02 06:25:44 +0000

    renamed default to index

--- app/View/templates/custom_routes/admin_home.php
+++ app/View/templates/custom_routes/admin_home.php
@@ -1,5 +1,5 @@
 <?php $payload->title = th('Extapi :: Home',false); ?>
 <h1>Get Portland Trimet arrival times through SMS</h1>
 <ul>
-<li><a href="/admin">Admin</a></li>
+<li><a href="/admin/users">Users</a></li>
 </ul>
